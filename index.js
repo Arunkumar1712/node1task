ef245405956dd8696cab24f05ab02e7905263181
Arunkumar1712 <arunk05691@gail.com>
2024-03-20 21:50:59 +0530

    update

--- index.js
+++ index.js
@@ -9,32 +9,33 @@ app.get('/', function (req, res) {
 
 app.listen(port, () => console.log("Server started")); 
 
-const currentDatetime = new Date();
-
-const year = currentDatetime.getUTCFullYear();
-const month = (currentDatetime.getUTCMonth() + 1).toString();
-const day = (currentDatetime.getUTCDate()).toString().padStart(2, '0');
-let hours = currentDatetime.getUTCHours();
-const amPm = (hours >= 12 ? 'PM' : 'AM');
-hours = (hours % 12 ).toString().padStart(2, '0'); // Convert hours to 12-hour format
-const minutes = (currentDatetime.getUTCMinutes()).toString().padStart(2, '0');
-const seconds = currentDatetime.getUTCSeconds().toString().padStart(2, '0');
-// Create the filename
-const filename = `${year}-${month}-${day}-${hours}-${minutes}-${seconds}-${amPm}.txt`;
-
-// Create the Timestamp 
-const data = `Current Timestamp - ${hours}:${minutes}:${seconds} ${amPm}`;
-console.log(data);
-
 app.get("/write",(req,res)=>{
+    const currentDatetime = new Date();
+
+    // Format the date and time
+    const year = currentDatetime.getFullYear();
+    const month = (currentDatetime.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDatetime.getDate().toString().padStart(2, '0');
+    let hours = currentDatetime.getHours();
+    const amPm = (hours >= 12 ? 'PM' : 'AM');
+    hours = (hours % 12 || 12).toString().padStart(2, '0'); // Convert hours to 12-hour format
+    const minutes = currentDatetime.getMinutes().toString().padStart(2, '0');
+    const seconds = currentDatetime.getSeconds().toString().padStart(2, '0');
     
+    // Create the filename
+    const filename = `${year}-${month}-${day}-${hours}-${minutes}-${seconds}-${amPm}.txt`;
+
+    // Create the Timestamp 
+    const data = `Current Timestamp - ${hours}:${minutes}:${seconds} ${amPm}`;
+    console.log(data);
+
     fs.writeFile(`./filesCreated/${filename}`, data, (err) => {
         if (err) {
             console.log("Error Occurred", err);
             res.send(`Error occurred while writing ${filename}.txt`);
         } else {
             console.log(`${filename} added`);
-            res.send(`${filename}.txt file added successfully`);
+            res.send(`${filename} file added successfully`); // Changed this line
         }
     });
 });
