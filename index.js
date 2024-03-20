2f0c05a212e1c2b0dc337e0a49162570b015b311
Arunkumar1712 <arunk05691@gail.com>
2024-03-20 21:46:57 +0530

    update

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ app.listen(port, () => console.log("Server started"));
 
 const currentDatetime = new Date();
 
-const year = currentDatetime.getFullYear();
-const month = (currentDatetime.getMonth() + 1).toString();
-const day = currentDatetime.getDate().toString().padStart(2, '0');
-let hours = currentDatetime.getHours();
-const amPm = hours >= 12 ? 'PM' : 'AM';
-hours = (hours % 12 || 12).toString().padStart(2, '0'); // Convert hours to 12-hour format
-const minutes = (currentDatetime.getMinutes()).toString().padStart(2, '0');
-const seconds = currentDatetime.getSeconds().toString().padStart(2, '0');
+const year = currentDatetime.getUTCFullYear();
+const month = (currentDatetime.getUTCMonth() + 1).toString();
+const day = (currentDatetime.getUTCDate()).toString().padStart(2, '0');
+let hours = currentDatetime.getUTCHours();
+const amPm = (hours >= 12 ? 'PM' : 'AM');
+hours = (hours % 12 ).toString().padStart(2, '0'); // Convert hours to 12-hour format
+const minutes = (currentDatetime.getUTCMinutes()).toString().padStart(2, '0');
+const seconds = currentDatetime.getUTCSeconds().toString().padStart(2, '0');
 // Create the filename
 const filename = `${year}-${month}-${day}-${hours}-${minutes}-${seconds}-${amPm}.txt`;
 
@@ -31,10 +31,10 @@ app.get("/write",(req,res)=>{
     fs.writeFile(`./filesCreated/${filename}`, data, (err) => {
         if (err) {
             console.log("Error Occurred", err);
-            res.send(`Error occurred while writing ${filename}`);
+            res.send(`Error occurred while writing ${filename}.txt`);
         } else {
             console.log(`${filename} added`);
-            res.send(`${filename}file added successfully`);
+            res.send(`${filename}.txt file added successfully`);
         }
     });
 });
