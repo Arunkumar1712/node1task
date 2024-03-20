6824e642151c36d53764522fb5abbb564940030d
Arunkumar1712 <arunk05691@gail.com>
2024-03-20 21:40:06 +0530

    updated

--- index.js
+++ index.js
@@ -1,51 +1,46 @@
 const express = require('express');
 const fs = require("fs");
 const app = express();
-const port =3000;
-
+const port = 3000;
 
 app.get('/', function (req, res) {
-  res.send('nodeJs file System')
-})
+  res.send('nodeJs file System');
+});
 
-app.listen(port,()=>console.log("server started")) 
+app.listen(port, () => console.log("Server started")); 
+
 const currentDatetime = new Date();
-
-
-
 
 const year = currentDatetime.getFullYear();
 const month = (currentDatetime.getMonth() + 1).toString();
-const day = (currentDatetime.getDate()).padStart(2, '0').toString();
+const day = currentDatetime.getDate().toString().padStart(2, '0');
 let hours = currentDatetime.getHours();
-const amPm =(hours >= 12 ? 'PM' : 'AM').padStart(2, '0');
-hours = (hours % 12 || 12).toString().padStart(2, '0');; // Convert hours to 12-hour format
+const amPm = hours >= 12 ? 'PM' : 'AM';
+hours = (hours % 12 || 12).toString().padStart(2, '0'); // Convert hours to 12-hour format
 const minutes = (currentDatetime.getMinutes()).toString().padStart(2, '0');
-const seconds = String(currentDatetime.getSeconds());
+const seconds = currentDatetime.getSeconds().toString().padStart(2, '0');
 // Create the filename
-const filename = `${year}-${month}-${day}-${hours}-${minutes}-${amPm}.txt`;
+const filename = `${year}-${month}-${day}-${hours}-${minutes}-${seconds}-${amPm}.txt`;
 
 // Create the Timestamp 
-const data=`Curent Timestamp - ${hours}-${minutes}-${seconds}-${amPm}`
+const data = `Current Timestamp - ${hours}:${minutes}:${seconds} ${amPm}`;
 console.log(data);
 
 app.get("/write",(req,res)=>{
     
-    fs.writeFile(`./filesCreated/${filename}`,data,(err)=>{
+    fs.writeFile(`./filesCreated/${filename}`, data, (err) => {
         if (err) {
             console.log("Error Occurred", err);
-          } else {
-            console.log(`${filename}Added`);
-          }
-          res.send(`${filename}.txt file added successfully`);
-    })
+            res.send(`Error occurred while writing ${filename}`);
+        } else {
+            console.log(`${filename} added`);
+            res.send(`${filename}file added successfully`);
+        }
+    });
+});
 
-
-
-})
 app.get("/read", (req, res) => {
     fs.readdir(`./filesCreated`, (err, files) => {
       res.send(`All file names are ${files}`);
     });
-  });
-
+});
